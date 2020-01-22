47d8dddf43b35ead6504c9b53f0c552091cd6282
matheuscostazup <matheus.costa@zup.com.br>
2020-01-22 11:02:19 -0300

    Add method to return a list of categories

--- src/main/java/com/udemy/spring/course/resources/CategoriaResource.java
+++ src/main/java/com/udemy/spring/course/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.udemy.spring.course.resources;
 
+import com.udemy.spring.course.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method=RequestMethod.GET)
-    public String listar(){
-        return "Rest está funcionando";
+    public List<Categoria> listar(){
+
+        Categoria categoria1 = new Categoria(1, "Informática");
+        Categoria categoria2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList();
+        lista.add(categoria1); lista.add(categoria2);
+
+        return lista;
     }
+
+
+
 
 }
