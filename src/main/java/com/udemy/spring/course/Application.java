1c1c2297b12eac4beaae505dfdec837136e3356a
Matheus José <matheus-costa@matheus.costa>
2020-01-23 20:20:24 -0300

    Save categories on database

--- src/main/java/com/udemy/spring/course/Application.java
+++ src/main/java/com/udemy/spring/course/Application.java
@@ -1,11 +1,30 @@
 package com.udemy.spring.course;
 
+import com.udemy.spring.course.domain.Categoria;
+import com.udemy.spring.course.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
     }
 }
