0966dadc7504d8df8411c4a8279be769ee4d95ba
matheuscostazup <matheus.costa@zup.com.br>
2020-05-30 15:51:53 -0300

    Aula 34 - Insere nova categoria

--- src/main/java/com/udemy/spring/course/Application.java
+++ src/main/java/com/udemy/spring/course/Application.java
@@ -12,7 +12,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Date;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
--- src/main/java/com/udemy/spring/course/resources/CategoriaResource.java
+++ src/main/java/com/udemy/spring/course/resources/CategoriaResource.java
@@ -5,9 +5,9 @@ import com.udemy.spring.course.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -22,6 +22,14 @@ public class CategoriaResource {
         Categoria obj = service.buscar(id);
 
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(method=RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
