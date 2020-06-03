7b5d643bacb4c97b31f3fb11053b5038f963e608
matheuscostazup <matheus.costa@zup.com.br>
2020-06-03 20:53:34 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/udemy/spring/course/resources/CategoryResource.java
+++ src/main/java/com/udemy/spring/course/resources/CategoryResource.java
@@ -4,6 +4,7 @@ import com.udemy.spring.course.domain.Category;
 import com.udemy.spring.course.dto.CategoryDTO;
 import com.udemy.spring.course.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -49,8 +50,18 @@ public class CategoryResource {
     @RequestMapping(method=RequestMethod.GET)
     public ResponseEntity<List<CategoryDTO>> findAll(){
         List<Category> list = service.findAll();
-        List<CategoryDTO> listDto = list.stream().map(obj -> new CategoryDTO(obj)).collect(Collectors.toList());
+        List<CategoryDTO> listDto = list.stream().map(CategoryDTO::new).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
     }
 
+    @RequestMapping(value="/page", method=RequestMethod.GET)
+    public ResponseEntity<Page<CategoryDTO>> findPage(
+            @RequestParam(value="page", defaultValue="0") Integer page,
+            @RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
+            @RequestParam(value="orderBy", defaultValue="name") String orderBy,
+            @RequestParam(value="direction", defaultValue="ASC") String direction){
+        Page<Category> list = service.findPage(page, linesPerPage, orderBy, direction);
+        Page<CategoryDTO> listDto = list.map(CategoryDTO::new);
+        return ResponseEntity.ok().body(listDto);
+    }
 }
--- src/main/java/com/udemy/spring/course/services/CategoryService.java
+++ src/main/java/com/udemy/spring/course/services/CategoryService.java
@@ -7,6 +7,9 @@ import com.udemy.spring.course.services.exception.DataIntegrityException;
 import com.udemy.spring.course.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,6 +31,11 @@ public class CategoryService {
 
     public List<Category> findAll(){
         return repository.findAll();
+    }
+    
+    public Page<Category> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return repository.findAll(pageRequest);
     }
 
     public Category insert(Category obj){
